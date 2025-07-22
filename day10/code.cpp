bf7e8e62f4cf06e0dbbb4912250d4b377959aa02
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-07-22 11:47:53 +0530

    feature1

--- day10/code.cpp
+++ day10/code.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {
     cout << "Day 10 C++ exercises" << endl;
     cout<<"This project uses the C++17 language standard version." << endl;
-    cout <<"change in main branch"<<endl;
+    cout <<"change in feature1 branch"<<endl;
     return 0;
 }
