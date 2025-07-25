acbaa5f224e16ac0cf91b2a25ed7db7535b75121
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-07-25 21:31:26 +0530

    Min

--- day10/Largest.cpp
+++ day10/Largest.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main(){
     int arr[] = {5,4,3,9,12};
     int largest = arr[0];
+    int min = arr[0];
+    // Initialize largest with the first element of the array
     for(int i = 1; i < 5; i++){
         if(arr[i] > largest){
             largest = arr[i];
         }
+        if(arr[i] < min){
+            min = arr[i];
+        }
     }
     cout << "The largest element is: " << largest << endl;
+    cout << "The smallest element is: " << min << endl;
     return 0;
 }
