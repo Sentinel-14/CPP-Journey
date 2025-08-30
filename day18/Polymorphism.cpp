1e5471dbdfea972e746ba7a1c98f1ad171b570bb
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-08-30 12:50:04 +0530

    Polymorphism

---
+++ day18/Polymorphism.cpp
@@ -0,0 +1,14 @@
+/*
+POLYMORPHISM
+Polymorphism is the ability of objects to take on different forms
+or behave in different ways depending on the context in 
+which they are used.
+-Compile Time Polymorphism
+-Run Time Polymorphism
+
+
+Basically constructor overloading is known as polymorphism
+Car()
+Car(name,color)
+Car(name)
+*/
