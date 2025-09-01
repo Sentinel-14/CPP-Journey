afb004f0aa0dfa98382216b5cea4100157f95793
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-09-01 16:30:58 +0530

    Sum of n natural numbe

---
+++ day19/Sumofn_naturalnumber.cpp
@@ -0,0 +1,19 @@
+//Sum of n natural numbers
+
+#include <iostream>
+using namespace std;
+int sum(int n){
+    if(n==1){
+        return 1;
+    }
+    int ans=n+sum(n-1);
+    return ans;
+
+}
+int main(){
+    int x;
+    cin>>x;
+    cout<<sum(x);
+    return 0;
+    
+}
