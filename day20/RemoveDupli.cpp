562048d4cea43b2ec61a65ce1b3b935f178ecf97
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-10-18 13:37:17 +0530

    latest

---
+++ day20/RemoveDupli.cpp
@@ -0,0 +1,28 @@
+//Remove Duplicate in String
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+void removeDuplicates(string str, string ans, int i, int map[26]){
+  if(i== str.size()){
+    cout<<"ans:"<<ans;
+    return;
+  }
+    char ch=str[i];
+    int mapIdx=(int)(ch-'a');
+    if(map[mapIdx]){
+        removeDuplicates(str,ans,i+1,map);
+    }else{
+        map[mapIdx]=true;
+        removeDuplicates(str,ans+str[i],i+1,map);
+    }
+
+}
+int main (){
+    string str="appnacollege";
+    string ans="";
+    int map[26]={false};
+
+    removeDuplicates(str,ans,0,map);
+    return 0;
+}
