3a4a5b8412ee5951e95bad58f5b0d68bbdfc6a09
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-10-22 14:15:53 +0530

    incomplete , very hard to understand

---
+++ day20/TowerofHanoi.cpp
@@ -0,0 +1,12 @@
+/* TOWER OF HANOI
+You have 3 towers and N disks of different sizes which can slide onto an tower. 
+The puzzle starts with disks sorted in ascending order of size from top to bottom (i.e., each disk sits on top os an even larger one).
+
+You have the following constraints:
+(1) Only one disk can be moved at a time.
+(2) A disk is slid off the top of one tower onto another tower.
+(3) A disk cannot be placed ontop of a smaller disk. Write a program to move the disks from the first tower to the last using stacks.
+*/
+#include <iostream>
+#include <string>
+using namespace std;
