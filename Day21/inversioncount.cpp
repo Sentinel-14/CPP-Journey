f7a90aad096e13e2cc83d9259ce2eec5e08984ec
Sentinel-14 <bhavnishgoel2005@gmail.com>
2025-10-23 19:52:00 +0530

    Inversion Count

---
+++ Day21/inversioncount.cpp
@@ -0,0 +1,14 @@
+//Given an array integers.Find the Inversion Count in the array.
+/*INVERSION COUNT:- for an array, inversion count indicates how far (or close)the array is 
+form being sorted. If the array is already sorted then the inversion count is 0. If an array 
+is sorted in tyhe reverse order then the inversion count is the maximum.
+Formally, two elements a[i] and a[j] form an inversion if a[i]>a[j] and i<j.*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int arr[]={2,4,1,3,5};
+    int n=5;
+}
